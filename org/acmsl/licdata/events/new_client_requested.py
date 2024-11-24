69a8c8a8b7be87e6a41b25f09819cc8184b31763
rydnr <github@acm-sl.org>
2024-11-24 20:55:23 +0100

    Initial events

---
+++ org/acmsl/licdata/events/new_client_requested.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+"""
+org/acmsl/licdata/events/new_client_requested.py
+
+This file defines the NewClientRequested class.
+
+Copyright (C) 2024-today ACM S.L. Licdata-Events
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from pythoneda.shared import Event
+from typing import List
+
+
+class NewClientRequested(Event):
+    """
+    Represents events for creating new Client instances.
+
+    Class name: NewClientRequested
+
+    Responsibilities:
+        - Represent the event when a new Client is requested.
+
+    Collaborators:
+        - None
+    """
+
+    def __init__(
+        self,
+        email: str,
+        address: str,
+        contact: str,
+        phone: str,
+        previousEventIds: List[str] = None,
+        reconstructedId: str = None,
+        reconstructedPreviousEventIds: List[str] = None,
+    ):
+        """
+        Creates a new NewClientRequested instance.
+        :param email: The email.
+        :type email: str
+        :param address: The address.
+        :type address: str
+        :param contact: The contact information.
+        :type contact: str
+        :param phone: The phone.
+        :type phone: str
+        """
+        super().__init__(
+            previousEventIds, reconstructedId, reconstructedPreviousEventIds
+        )
+        self._email = email
+        self._address = address
+        self._contact = contact
+        self._phone = phone
+
+    @property
+    def email(self) -> str:
+        """
+        Retrieves the email.
+        :return: Such email.
+        :rtype: str
+        """
+        return self._email
+
+    @property
+    def address(self) -> str:
+        """
+        Retrieves the address.
+        :return: Such address.
+        :rtype: str
+        """
+        return self._address
+
+    @property
+    def contact(self) -> str:
+        """
+        Retrieves the contact.
+        :return: Such contact.
+        :rtype: str
+        """
+        return self._contact
+
+    @property
+    def phone(self) -> str:
+        """
+        Retrieves the phone.
+        :return: Such phone.
+        :rtype: str
+        """
+        return self._phone
+
+
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
